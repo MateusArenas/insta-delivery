aa0e6db113c3a375d4543be5b7c8b86d79cb4fc4
MateusArenas <MateusArenas97@gmail.com>
2022-08-01 23:28:39 -0300

    all

---
+++ components/HeaderDesktopOnly.tsx
@@ -0,0 +1,147 @@
+import { Button, Container, Form, Nav, Navbar, NavDropdown, Dropdown, FormControlProps } from 'react-bootstrap';
+import { IoBagOutline, IoEnterOutline } from 'react-icons/io5';
+import { MdExpandMore, MdPersonOutline, MdHelpOutline, MdChevronRight, MdOutlineAccountCircle, MdHistory } from 'react-icons/md';
+import { RiSearchLine } from 'react-icons/ri';
+
+import { useRouter } from 'next/router';
+
+import Link from 'next/link'
+import React from 'react'
+
+// eslint-disable-next-line react/display-name
+const CustomToggleAndInputSearch = React.forwardRef(({ onClick, ...props }: any, ref) => ( 
+    <Form.Control {...props}
+      type='search'
+      placeholder="Busque por item ou loja"
+      className="me-2 ps-5"
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+      onFocus={(e) => {
+        e.preventDefault();
+        setTimeout(() => {
+          onClick(e);
+        }, 300);
+      }}
+    />
+));
+
+function HeaderDesktopOnly(props: any) {
+
+  const router = useRouter()
+
+
+  return (
+    <Navbar sticky='top' bg='white' className='border-bottom d-none d-md-block align-items-center header-height' collapseOnSelect expand="md">
+      <Container>
+        
+        <Link href='/'>
+          <Navbar.Brand href='/'>
+            insta-delivery
+          </Navbar.Brand>
+        </Link> 
+
+          <Nav className="">
+
+            <Nav.Link className='' onClick={props?.onOpenModal}>
+              <span>Próximo de Jardim Maria Eneida</span>
+              <MdExpandMore />
+            </Nav.Link>
+{/* 
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+            
+            
+          </Nav>
+
+          <Form onSubmit={() => console.log('search')} className="d-flex align-items-center flex-grow-1 ms-3 me-5">
+            <Dropdown className='w-100' >
+
+                <Button type='submit' variant="link" className="position-absolute border-0 text-dark">
+                  <RiSearchLine style={{ marginTop: -4 }} />
+                </Button>
+
+                <Dropdown.Toggle 
+                  as={CustomToggleAndInputSearch} 
+                  id="dropdown-custom-components" 
+                />
+              
+                <Dropdown.Menu className='w-100' >
+                  <Dropdown.Header>Buscas recentes</Dropdown.Header>
+                  <Button type='submit' variant="link" className="dropdown-item py-2 text-decoration-none border-0 text-dark">
+                    <MdHistory />
+                    <span className='mx-3 me-5'>d</span>
+                  </Button>
+                  <Button type='submit' variant="link" className="dropdown-item py-2 text-decoration-none border-0 text-dark">
+                    <MdHistory />
+                    <span className='mx-3 me-5'>hamburger</span>
+                  </Button>
+                </Dropdown.Menu>
+            </Dropdown>
+          </Form>
+
+          <Nav>
+            {/* <Nav.Link href="#deets" className='not-arrow mx-2'> */}
+
+            {/* <IoEnterOutline size={24} /> */}
+              <NavDropdown className='not-arrow mx-2' title={<MdOutlineAccountCircle size={24} />}  id="collasible-nav-dropdown">
+                <div className="p-4">
+                  <NavDropdown.Header>
+                    <h5>Olá Mateus</h5>
+                  </NavDropdown.Header>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item className='d-flex flex-row align-items-center' href="#action/3.1">
+                    <MdHelpOutline />
+                    <span className='mx-3 me-5'>Ajuda</span>
+                    <MdChevronRight />
+                  </NavDropdown.Item>
+                  <NavDropdown.Item className='d-flex flex-row align-items-center' href="#action/3.2">
+                    <MdPersonOutline />
+                    <span className='mx-3 me-5'>Entrar</span>
+                    <MdChevronRight />
+                  </NavDropdown.Item>
+                </div>
+              </NavDropdown>
+
+            {/* </Nav.Link> */}
+
+
+            <Link 
+              href={router.query?.['cart'] ? router.pathname : `/?cart=${'cart'}`}
+              as={router.query?.['cart'] ? router.pathname : `/cart`}
+              // eventKey={2} 
+            >
+              <Nav.Link 
+                disabled={router.pathname === '/cart'} 
+                href={router.query?.['cart'] ? router.pathname : `/cart`}  
+                active={router.pathname === '/cart'}
+                className={`d-flex align-items-center mx-2`}
+              >
+                <IoBagOutline size={24} />
+                <div className={`d-flex flex-column ms-1`}>
+                  <small style={{ fontSize: 12, fontWeight: 'bold' }}>R$ 0,00</small>
+                  <small style={{ fontSize: 10, fontWeight: '500', marginTop: -4 }}>0 items</small>
+                </div>
+              </Nav.Link>
+            </Link>
+
+          </Nav>
+
+
+      </Container>
+    </Navbar>
+  );
+}
+
+export default HeaderDesktopOnly;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,85 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import React, { ReactElement, ReactNode } from 'react'
+import Head from 'next/head'
+
+import { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+import Link from 'next/link'
+import HeaderDesktopOnly from '../components/HeaderDesktopOnly';
+import { Offcanvas, CloseButton, Container, Modal, Button } from 'react-bootstrap';
+
+import Cart from './cart'
+import { useRouter } from 'next/router';
+
+type NextPageWithLayout = NextPage & { getLayout?: (page: ReactElement) => ReactNode }
+type AppPropsWithLayout = AppProps & { Component: NextPageWithLayout }
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const [show, setShow] = React.useState(false);
+  const [modalShow, setModalShow] = React.useState(false);
+
+  const router = useRouter()
+
+  const handleClose = () => {
+    setShow(false)
+    router.push(router.pathname)
+  };
+  const handleToggle = () => setShow(!show);
+
+  const getLayout = Component.getLayout ?? ((page) => page) // Use the layout defined at the page level, if available
+
+
+  React.useEffect(() => {
+    if (router.query?.cart) {
+      setShow(true)
+    } else {
+      setShow(false)
+    }
+  }, [router.query])
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <HeaderDesktopOnly onOpenOffcanvas={handleToggle} onOpenModal={() => setModalShow(!modalShow)} />
+
+      {getLayout(<Component {...pageProps} />)}
+
+      <Modal size="lg" aria-labelledby="contained-modal-title-vcenter" centered 
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      >
+        <Modal.Header className='border-0 mb-4' closeButton />
+        <Modal.Body>
+          <h4 className="text-center" >Onde você quer receber seu pedido?</h4>
+
+          <div className="text-center mb-5">
+            <p>Já tem um endereço salvo?</p>
+            <p>Entre na sua conta para selecionar seu endereço.</p>
+          </div>
+
+          <div className="d-flex justify-content-center mb-5">
+            <Link href={'#'}>
+              <a className='p-1 text-primary fw-semibold text-decoration-none'>Entrar ou cadastrar</a>
+            </Link>
+          </div>
+        </Modal.Body>
+      </Modal>
+
+      <Offcanvas tabindex className='header-offset' backdropClassName='header-offset' show={show} onHide={handleClose} placement={'end'}>
+        <div className='d-flex flex-row'>
+          <CloseButton className='m-3 p-2' onClick={handleClose} />
+
+          <div className="d-flex flex-column mt-5">
+            <Cart />
+          </div>
+        </div>
+      </Offcanvas>
+    </>
+  )
 }
 
 export default MyApp
