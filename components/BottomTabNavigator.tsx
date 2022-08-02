a2144516cb42f2625bb58a0519175c1498cd50e2
MateusArenas <MateusArenas97@gmail.com>
2022-08-02 04:11:44 -0300

    all

--- components/BottomTabNavigator.tsx
+++ components/BottomTabNavigator.tsx
@@ -12,7 +12,7 @@ const BottomTabNavigator: React.FC = () => {
           <div className="col-3 text-center">
             <Link href={'/'}>
               <Nav.Link href="/">
-                <MdHome className='w-100' size={24} />
+                <MdHome style={{ marginTop: -2 }} className='w-100' size={24} />
                 <div style={{ fontSize: 10, marginTop: -4 }}>Home</div>
               </Nav.Link>
             </Link>
@@ -20,7 +20,7 @@ const BottomTabNavigator: React.FC = () => {
           <div className="col-3 text-center">
             <Link href={'/search'}>
               <Nav.Link href="/search">
-                <MdSearch className='w-100' size={24} />
+                <MdSearch style={{ marginTop: -2 }} className='w-100' size={24} />
                 <div style={{ fontSize: 10, marginTop: -4 }}>Search</div>
               </Nav.Link>
             </Link>
@@ -28,7 +28,7 @@ const BottomTabNavigator: React.FC = () => {
           <div className="col-3 text-center">
             <Link href={'/cart'}>
               <Nav.Link href="/cart">
-                <MdShoppingCart className='w-100' size={24} />
+                <MdShoppingCart style={{ marginTop: -2 }} className='w-100' size={24} />
                 <div style={{ fontSize: 10, marginTop: -4 }}>Cart</div>
               </Nav.Link>
             </Link>
@@ -36,7 +36,7 @@ const BottomTabNavigator: React.FC = () => {
           <div className="col-3 text-center">
             <Link href={'/account'}>
               <Nav.Link href="/account">
-                <MdAccountCircle className='w-100' size={24} />
+                <MdAccountCircle style={{ marginTop: -2 }} className='w-100' size={24} />
                 <div style={{ fontSize: 10, marginTop: -4 }}>Account</div>
               </Nav.Link>
             </Link>
